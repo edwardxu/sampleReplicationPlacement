2611c6296e1fbce5cd400998113e81eb3207b73e
edwardxu <uceexxx@supports-MacBook-Pro.local>
2016-09-30 12:02:26 +0100

    adjust parameters

--- src/simulation/Parameters.java
+++ src/simulation/Parameters.java
@@ -14,9 +14,9 @@ public class Parameters {
 	
 	//Data centre resource capacity ranges
 	final public static double maxComputingPerDC = 2000;//GHz  //the maximum computing capacity of a data center
-	final public static double minComputingPerDC = 500;//1000 GHz  //the minimum computing capacity of a data center
+	final public static double minComputingPerDC = 100;//1000 GHz  //the minimum computing capacity of a data center
 
-	final public static double minLinkDelay = 10; //ms
+	final public static double minLinkDelay = 50; //ms
 	final public static double maxLinkDelay = 100; //ms 50
 	
 	final public static double maxBandwidthPerLink = 1000;//1;//Gbps //the maximum network capacity of an inter-datacentre link
@@ -43,8 +43,8 @@ public class Parameters {
 	final public static int numOfDatasetPerQueryMax = 3;
 	final public static int numOfDatasetPerQueryMin = 1;
 	
-	final public static double queryDelayRequirementMax = 2000; // ms 1000
-	final public static double queryDelayRequirementMin = 300;// ms 
+	final public static double queryDelayRequirementMax = 1000; // ms 2000
+	final public static double queryDelayRequirementMin = 500;// ms 300
 	
 	//final public static int lifeCycleMax = (int) ((int) numOfTrials * 0.2); 
 	//final public static int lifeCycleMin = (int) ((int) numOfTrials * 0.02);
@@ -57,7 +57,7 @@ public class Parameters {
 	final public static double processCostUnitDataMax = 0.1;//0.22 $ per GB data // c_p(v_i) 
 	final public static double processCostUnitDataMin = 0.05;//0.15 $ per GB data // c_p(v_i)
 	
-	final public static double computingAllocatedToUnitData = 20;//10 GHz $ per GB //r_c
+	final public static double computingAllocatedToUnitData = 50;//10 GHz $ per GB //r_c
 	
 	final public static double bandwidthAllocatedToUnitData = 0.065;//Gbps $ per GB //r_t
 	
